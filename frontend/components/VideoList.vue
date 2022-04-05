34ee7da8734c00c7ba70b1eba641fe6684124293
aipa <k.tamashiro@churadata.okinawa>
2022-04-05 11:09:49 +0900

    [modify] pageからロジックを分離.

---
+++ frontend/components/VideoList.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div v-for="k in this.$store.getters['videos/getVideos']" :key="k.url">
+      <iframe
+        :src="k.url"
+        rel="preload"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <p>{{ video.like }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoList',
+  data: function () {
+    return {
+      video: {},
+      startX: 0,
+      moveX: 0,
+      width: 0,
+    }
+  },
+  methods: {
+    next() {
+      this.$store.dispatch('videos/videoShift').then(
+        (res) => {
+          this.video = res
+        },
+        (error) => {
+          console.error(error)
+        }
+      )
+    },
+    touchStart(e) {
+      this.width = this.$refs.swipe.offsetWidth
+      this.startX = e.touches[0].pageX
+    },
+    touchMove(e) {
+      this.moveX = e.touches[0].pageX - this.startX
+    },
+    touchEnd() {
+      if (this.moveX > 10) {
+        console.log('右スワイプ')
+        this.next()
+      } else if (this.moveX < -10) {
+        console.log('左スワイプ')
+      }
+    },
+  },
+  mounted() {
+    const videos = this.$store.getters['videos/getVideos']
+    this.video = videos[0]
+  },
+}
+</script>
+
+<style scoped>
+.iframe-wrapper {
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.iframe-wrapper iframe {
+  position: absolute;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50%;
+}
+</style>
