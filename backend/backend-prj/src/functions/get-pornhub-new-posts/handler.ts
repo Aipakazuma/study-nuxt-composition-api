381d70a4b2495da8c78422f2e32634bb220267d6
aipa <k.tamashiro@churadata.okinawa>
2022-04-21 16:55:31 +0900

    [modify] チュートリアル中.

--- backend/backend-prj/src/functions/get-pornhub-new-posts/handler.ts
+++ backend/backend-prj/src/functions/get-pornhub-new-posts/handler.ts
@@ -1,10 +1,24 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import * as superagent from 'superagent';
+import * as cheerio from 'cheerio';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<void> = async () => {
+
+  // const url: string = 'https://jp.pornhub.com/';
+  // const url: string = 'https://www.yahoo.co.jp/';
+  const url: string = 'https://www.xvideos.com/lang/japanese';
+  const result = await superagent.get(url);
+  const $ = cheerio.load(result.text);
+  const items = $('.mozaique > div');
+  const resArray = items.map((_, ele) => {
+    return $(ele).attr()['data-id'];
+  }).toArray();
+
   return formatJSONResponse({
-    message: `Hello. welcome to the porn Serverless world!`
+    message: `Hello.`,
+    dataIdList: JSON.stringify(resArray)
   });
 };
 
