baede87b8e608e6996b18b666956795503eef27a
aipa <k.tamashiro@churadata.okinawa>
2022-05-13 02:21:22 +0900

    [add] 雑にページを追加.

---
+++ frontend/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <div>
+            <section>
+                <h2>このサイトについて</h2>
+                <p>PWAやNuxtを勉強するために開設しました。</p>
+            </section>
+            <section>
+                <h2>お問い合わせ</h2>
+                <p>何かありましたら下記リンクからお問い合わせしてください。</p>
+                <a class="button" href="/contact/">お問い合わせ</a>
+            </section>
+        </div>
+    </div>
+</template>
